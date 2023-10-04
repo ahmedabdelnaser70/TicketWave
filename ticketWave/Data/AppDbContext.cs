e0044dbe8601cf1a967404ec51b2d80051f3311f
Ahmed Abdelnaser <112420984+ahmedabdelnaser70@users.noreply.github.com>
2023-10-05 01:56:30 +0300

    add table

--- ticketWave/Data/AppDbContext.cs
+++ ticketWave/Data/AppDbContext.cs
@@ -11,6 +11,8 @@ namespace ticketWave.Data
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> orderItems { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- ticketWave/Models/OrderItem.cs
+++ ticketWave/Models/OrderItem.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public int Amount { get; set; }
-        public decimal Price { get; set; }
+        public double Price { get; set; }
 
         public int MovieId { get; set; }
         public Movie Movie { get; set; }
