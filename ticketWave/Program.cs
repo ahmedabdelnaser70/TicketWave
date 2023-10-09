2a96533c1b3a6e2e38fd8015faca77c48596167d
Ahmed Abdelnaser <112420984+ahmedabdelnaser70@users.noreply.github.com>
2023-10-09 22:13:21 +0300

    OrdersService

--- ticketWave/Program.cs
+++ ticketWave/Program.cs
@@ -23,6 +23,7 @@ namespace ticketWave
             builder.Services.AddScoped<IProducersService, ProducersService>();
             builder.Services.AddScoped<ICinemasService, CinemasService>();
             builder.Services.AddScoped<IMoviesService, MoviesService>();
+            builder.Services.AddScoped<IOrdersService, OrdersService>();
 
             builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             builder.Services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
