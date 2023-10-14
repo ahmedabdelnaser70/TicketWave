69aeef71bf265d33f6db70af632775ad0f366609
Ahmed Abdelnaser <112420984+ahmedabdelnaser70@users.noreply.github.com>
2023-10-15 02:34:06 +0300

    AccountController

---
+++ ticketWave/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ticketWave.Data;
+using ticketWave.Models;
+
+namespace ticketWave.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
