85d178ff1122e7d43cbe22de76f28e49a2b441e9
Ahmed Abdelnaser <112420984+ahmedabdelnaser70@users.noreply.github.com>
2023-10-16 15:39:29 +0300

    Register

--- ticketWave/Controllers/AccountController.cs
+++ ticketWave/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Win32;
 using ticketWave.Data;
+using ticketWave.Data.Static;
 using ticketWave.Data.ViewModels;
 using ticketWave.Models;
 
@@ -56,5 +57,32 @@ namespace ticketWave.Controllers
         }
 
         public IActionResult Register() => View(new RegisterVM());
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return View("RegisterCompleted");
+        }
     }
 }
